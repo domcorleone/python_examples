d38650e3157477898ed204d69fa80b39ba9f28d1
Delcio Machadp <delciomachado@gmail.com>
2022-10-13 19:46:37 +0100

    Add 3 ways of writing using single quotes...

--- exemple01.py
+++ exemple01.py
@@ -1,5 +1,8 @@
-print('Hello World...')
-print("Double Quotes")
-print('Concatena'+'tion')
-print('Hello', 'There')
-print('item'+5)
+# 3 ways of writing a string with single quote
+# First
+print('I\'m 5')
+# Second
+print("I'm 5")
+# Third
+print("He said 'Hi'")
+print('He said "HI"')
