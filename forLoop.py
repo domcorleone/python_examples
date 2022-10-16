a4f00dff9cbe173b86df8e41e43371127c6f5ffc
Delcio Machadp <delciomachado@gmail.com>
2022-10-16 14:12:05 +0100

    The file forLoop.py was modified

--- forLoop.py
+++ forLoop.py
@@ -1,4 +1,9 @@
-exampleList = [1,6,9,5,7,0]
+exampleList = [1,6,9,5,7,0,9,76]
 
 for thing in exampleList:
     print(thing)
+
+# for loop write less lines than while lopp
+print("new for loop will begin in this line...")
+for x in range(1,20):
+    print(x)
