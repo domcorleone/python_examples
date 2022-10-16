b32a8c7d0f41eeb76a0aeda1e4973fc10f8a38f9
Delcio Machadp <delciomachado@gmail.com>
2022-10-16 13:40:55 +0100

    Added loop.py to the project

---
+++ loop.py
@@ -0,0 +1,27 @@
+# studing comments
+"""
+    comment for
+    multiple
+    lines
+"""
+'''
+count = 6
+print("i am inside the python module")
+while( count > 1 ):
+    print("Your count number is:'"+str(count)+"'")
+    count = count -1
+'''
+
+count = 1
+
+while count < 10:
+    print(count)
+    # count = count + 1
+    count +=1
+
+# infinite loop
+
+while True:
+    print("Infinite Loop...")
+    
+    
