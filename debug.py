5be22596a4ccfc5f3d97da57ae6eba4db486d274
Eder L. Machado <eder.lourenco.machado@gmail.com>
2022-10-18 22:02:13 +0100

    Added debug.py file

---
+++ debug.py
@@ -0,0 +1,42 @@
+"""
+    Author: Eder L. Machado
+    Description: Debugging
+    Date: 18.10.2022 20:11
+"""
+# it will throw NameError | it is an exception
+# and program will run untill hits the exception
+print("Hello world...")
+"""
+variable = 55
+print(varaiable)
+"""
+# expected an idented block | it is a sintax error and will prevent the program
+# from running even if before the sintax error there is a line of code without
+# any problem at all
+"""
+def funct01():
+def funct02():
+    print("Hello World!!!")
+"""
+
+# Unexpected ident:
+"""
+def task():
+    print('1')
+print('2')
+
+    print('3')
+"""
+
+# EOF error
+#print('Hey there...how are you today'
+
+x = [1,20,45,8,96,12
+
+
+      
+
+
+
+
+    
