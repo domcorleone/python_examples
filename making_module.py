c33b84706dc1fff3cd733516b8edd6a8cb4df155
Eder L. Machado <eder.lourenco.machado@gmail.com>
2022-10-19 20:06:41 +0100

    Make changes to making and testing module files

---
+++ making_module.py
@@ -0,0 +1,8 @@
+"""
+    Author: Eder Machado
+    Description: Making module in python 3.x
+    Created Date: 19.10.2022 06:50 PM
+"""
+
+def exampleFunc(data):
+    print(data)
---
+++ testing_module.py
@@ -0,0 +1,12 @@
+"""
+    Author: Eder Machado
+    Description: Testing module
+    Created Date: 19.10.2022 06:55 PM
+"""
+
+import making_module
+
+making_module.exampleFunc("Hello There...")
+
+def somethingElse():
+    pass
