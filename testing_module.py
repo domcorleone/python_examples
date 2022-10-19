4c4fdb42ae561d405f5883ce9a3a4f04de51b18b
Eder L. Machado <eder.lourenco.machado@gmail.com>
2022-10-19 20:16:00 +0100

    Add comment about 'pass' use in python 3.x

--- testing_module.py
+++ testing_module.py
@@ -8,5 +8,8 @@ import making_module
 
 making_module.exampleFunc("Hello There...")
 
+# 'pass' is used to prevent error in situation when a function
+# or class will receive a peace of code in the future
+
 def somethingElse():
     pass
