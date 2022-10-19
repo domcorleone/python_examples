df888a0406f85dbf97e8df32e114e4b18e3ee4b5
Eder L. Machado <eder.lourenco.machado@gmail.com>
2022-10-19 13:39:39 +0100

    Updated classes.py file

---
+++ classes.py
@@ -0,0 +1,23 @@
+"""
+    author: Eder Machado
+    Description: Creating class in python
+    Create Date: 19.10.2022 12:25 PM
+"""
+
+class calc:
+    
+    def add(x,y):
+        answer = x + y
+        print(answer)
+
+    def mult(x,y):
+        answer = x * y
+        print(answer)
+
+    def sub(x,y):
+        answer = x - y
+        print(answer)
+
+    def div(x,y):
+        answer = x / y
+        print(answer)
